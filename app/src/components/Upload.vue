2108d24285674165c33a2b3859334edf8e62e026
Sergey Pavlov <self@sergeypavlov.dev>
2023-04-15 18:41:33 +0300

    Add image submission feedback

--- app/src/components/Upload.vue
+++ app/src/components/Upload.vue
@@ -1,16 +1,71 @@
 <script setup lang="ts">
 import {ref} from "vue";
 
+import * as T from "😺/types";
 import config from "😺/core/config";
 
-let file = ref<File | null>(null);
-const imageUrl = `${config.get("BACKEND_API_URL")}/image`;
+const file = ref<File | null>(null);
+const processing = ref(false);
+const isError = ref(false);
+const showAlert = ref(false);
+const alertText = ref<string | undefined>(undefined);
+const timeout = 5;
+
+const imageURL = `${config.get("BACKEND_API_URL")}/image`;
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
 async function onUpdate(files: File[]) {
+    isError.value = false;
+    showAlert.value = false;
+    alertText.value = undefined;
     if (files.length === 0) return;
     const first = files[0];
-    if (first !== undefined) file = first;
-    console.log(file);
+    if (first !== undefined) file.value = first;
+}
+
+async function onSubmit() {
+    if (file.value === null) return;
+
+    processing.value = true;
+    const payload = new FormData();
+    payload.append("data", file.value);
+
+    const response = await fetch(imageURL, {method: "POST", body: payload});
+
+    if (response.ok) {
+        const data: T.Image = await response.json();
+        const uid = data.uid;
+        let tries = 0;
+
+        while (tries < timeout) {
+            tries++;
+            const response = await fetch(`${imageURL}/${uid}`);
+            const data: T.Image = await response.json();
+
+            if (data.category !== null) {
+                processing.value = false;
+                alertText.value = `Predicted category: ${data.category.title}`;
+                file.value = null;
+                showAlert.value = true;
+                break;
+            }
+
+            await sleep(1000);
+        }
+
+        if (processing.value) {
+            processing.value = false;
+            alertText.value = `Could not get a prediction within ${timeout} second(s)`;
+            isError.value = true;
+            showAlert.value = true;
+        }
+    } else {
+        const err: T.APIError = await response.json();
+        processing.value = false;
+        isError.value = true;
+        alertText.value = err.detail;
+        showAlert.value = true;
+    }
 }
 </script>
 
@@ -23,16 +78,25 @@ async function onUpdate(files: File[]) {
                 label="Image"
                 prepend-icon="mdi-camera"
                 variant="solo"
+                hide-details
                 @update:model-value="onUpdate"
             ></v-file-input>
         </v-col>
         <v-responsive width="100%"></v-responsive>
-        <v-col cols="9">
-            <v-alert density="compact" type="error"></v-alert>
+        <v-col cols="9" :hidden="!showAlert">
+            <v-alert
+                v-model:model-value="showAlert"
+                closable
+                :icon="isError ? 'mdi-emoticon-cry' : 'mdi-check-circle'"
+                density="compact"
+                :type="isError ? 'error' : 'success'"
+                :title="isError ? 'Error' : 'Success'"
+                :text="alertText"
+            ></v-alert>
         </v-col>
         <v-responsive width="100%"></v-responsive>
         <v-col cols="9">
-            <v-btn block>Submit</v-btn>
+            <v-btn block :loading="processing" @click.prevent="onSubmit">Submit</v-btn>
         </v-col>
     </v-row>
 </template>
---
+++ app/src/types/index.ts
@@ -0,0 +1,44 @@
+export interface Category {
+    uid: number;
+    title: string;
+}
+
+export type ColorRGB =
+    | "#FF0000" // red
+    | "#FF8000"
+    | "#FFFF00" // yellow
+    | "#80FF00"
+    | "#00FF00" // green
+    | "#00FF80"
+    | "#00FFFF" // cyan
+    | "#0080FF"
+    | "#0000FF" // blue
+    | "#8000FF"
+    | "#FF00FF" // magenta
+    | "#FF0080";
+
+export type ColorRYB =
+    | "#FE2712" // red
+    | "#FC600A" // red-orange
+    | "#FB9902" // orange
+    | "#FCCC1A" // yellow-orange
+    | "#FEFE33" // yellow
+    | "#B2D732" // yellow-green
+    | "#66B032" // green
+    | "#347C98" // blue-green
+    | "#0247FE" // blue
+    | "#4424D6" // blue-purple
+    | "#8601AF" // purple
+    | "#C21460"; // red-purple
+
+export interface Image {
+    uid: number;
+    path: string;
+    category: Category | null;
+    color_RGB: ColorRGB | null;
+    color_RYB: ColorRYB | null;
+}
+
+export interface APIError {
+    detail: string;
+}
